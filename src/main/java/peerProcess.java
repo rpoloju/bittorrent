d21b2b5413e1b65f3619acc115cb79e2125a5ecb
washington.garcia <w.garcia@ufl.edu>
2018-03-22 23:02:30 -0400

    Update local meta data to use peer info singleton, remove port number math.

--- src/main/java/peerProcess.java
+++ src/main/java/peerProcess.java
@@ -11,7 +11,7 @@ public class peerProcess
     private int peerId;
     private int port;
     private String hostname;
-    private Boolean hasfile;
+    private int hasfile;
     private ArrayList<Boolean> bitfield;
     // private int bitfield;
     private DuplexServer listener;
@@ -25,15 +25,23 @@ public class peerProcess
     private void start(String[] args) throws Exception
     {   
         // read Common.cfg
+        SingletonCommon common_cfg = SingletonCommon.getInstance();
+
         peerId =  Integer.parseInt(args[0]);
+        
+        // read PeerInfo.cfg
+        SingletonPeerInfo peer_cfg = SingletonPeerInfo.getInstance();
+        RemotePeerInfo my_info = peer_cfg.peerInfoMap.get(peerId);
+
         // load in peer meta data here
-        port = 6000 + peerId;
-        hostname = "localhost";
-        hasfile = true;
+        port = my_info.getPeerPortNumber();
+        hostname = my_info.getPeerHostName();
+        hasfile = my_info.getHasFile_or_not();
+        System.out.println("CONFIG: " + hostname + ":" + port + " hasfile: " + hasfile);
         // we have 306 pieces (FileSize // PieceSize)
         // if (hasfile) { bitfield = 0b1111111111111111; }//{ bitfield = Integer.MAX_VALUE; }
         // else { bitfield = 0b0000000000000000; }
-        if (hasfile) { bitfield = new ArrayList<Boolean>(Collections.nCopies(306, true)); }
+        if (hasfile > 0) { bitfield = new ArrayList<Boolean>(Collections.nCopies(306, true)); }
         else { bitfield = new ArrayList<Boolean>(Collections.nCopies(306, false));}
         // start server to listen for messages. 
         listener = new DuplexServer(port);
@@ -45,7 +53,8 @@ public class peerProcess
             for (int i = 0; i < peers_to_connect; i++)
             {
                 // start clients to talk with previous peers
-                listener.init_socket(hostname, 6000 + 1001 + i, 0);
+                RemotePeerInfo prev_peer = peer_cfg.peerInfoMap.get(1001 + i);
+                listener.init_socket(prev_peer.getPeerHostName(), prev_peer.getPeerPortNumber(), 1001 + i);
                 // System.out.println("Broadcasting...");
                 // listener.broadcast_to_peers("I am here");
             }           
